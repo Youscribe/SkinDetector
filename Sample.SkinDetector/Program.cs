2e64e3431087fbd86e3d81ef7b06e0f552786441
Yann <yann.robin@youscribe.com>
2013-04-18 16:29:26 +0200

    First commit

---
+++ Sample.SkinDetector/Program.cs
@@ -0,0 +1,39 @@
+﻿using SkinDetector;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sample.SkinDetector
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var path = "img";
+
+            if (args.Length > 0)
+                path = args[0];
+
+            var files = Enumerable.Empty<string>();
+            FileAttributes attr = File.GetAttributes(path);
+
+            //detect whether its a directory or file
+            if ((attr & FileAttributes.Directory) == FileAttributes.Directory)
+                files = Directory.GetFiles(path);
+            else
+                files = new[] { path };
+
+            var skinDetectorService = new SkinDetectorService();
+            foreach (var file in files)
+            {
+                Console.WriteLine("file " + file);
+                var percent = skinDetectorService.GetPercentageOfSkinInImage(file);
+                Console.WriteLine(percent + "% skin detected");
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ SkinDetector/SkinDetectorService.cs
@@ -0,0 +1,61 @@
+﻿using OpenCvSharp;
+using OpenCvSharp.CPlusPlus;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SkinDetector
+{
+    public class SkinDetectorService
+    {
+        protected int CountSkinPoints(IplImage imgHueMask, CvColor color)
+        {
+            int count = 0;
+            for (int y = 0; y < imgHueMask.Height; y++)
+            {
+                for (int x = 0; x < imgHueMask.Width; x++)
+                {
+                    byte value = (byte)imgHueMask[y, x].Val0;
+                    if (value != 0)
+                    {
+                        count++;
+                        imgHueMask[y, x] = color;
+                    }
+                }
+            }
+            return count;
+        }
+
+        public double GetPercentageOfSkinInImage(string filePath, string debugImageFilePath = null)
+        {
+            using (IplImage imgSrc = new IplImage(filePath))
+            {
+                using (CvMemStorage storage = new CvMemStorage())
+                {
+                    CvSeq<CvPoint> points;
+                    using (IplImage imgGray = new IplImage(filePath, LoadMode.GrayScale))
+                    {
+                        imgGray.FindContours(storage, out points, CvContour.SizeOf, ContourRetrieval.External, ContourChain.ApproxNone);
+                    }
+                    var rect = Cv.BoundingRect(points);
+                    if (rect.Width < imgSrc.Width * 0.10 || rect.Height < imgSrc.Height * 0.10)
+                        rect = new CvRect(0, 0, imgSrc.Width, imgSrc.Height);
+                    using (var subImg = imgSrc.GetSubImage(rect))
+                    {
+                        using (IplImage imgHueMask = new IplImage(subImg.Size, BitDepth.U8, 1))
+                        {
+                            CvAdaptiveSkinDetector detector = new CvAdaptiveSkinDetector(1, MorphingMethod.ErodeDilate);
+                            detector.Process(subImg, imgHueMask);
+                            int count = CountSkinPoints(imgHueMask, CvColor.White);
+                            var percent = ((double)count / (double)(subImg.Width * subImg.Height) * 100);
+                            if (debugImageFilePath != null) imgHueMask.SaveImage(debugImageFilePath);
+                            return percent;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
