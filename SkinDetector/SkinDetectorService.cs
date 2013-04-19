f040ec1aa37c26291e47315df0d928f07bf53779
Yann <yann.robin@youscribe.com>
2013-04-19 13:37:07 +0200

    change the way we find border, very naive function

--- SkinDetector/SkinDetectorService.cs
+++ SkinDetector/SkinDetectorService.cs
@@ -26,18 +26,46 @@ namespace SkinDetector
             return count;
         }
 
+        private CvRect FindBorder(IplImage image)
+        {
+            byte color = (byte)image[0, 0].Val0;
+            var minY = image.Height;
+            var minX = image.Width;
+            var maxY = 0;
+            var maxX = 0;
+            for (int y = 0; y < image.Height; y++)
+            {
+                for (int x = 0; x < image.Width; x++)
+                {
+                    var currentColor = (byte)image[y, x].Val0;
+                    if (color != currentColor)
+                    {
+                        if (x < minX) minX = x;
+                        if (x > maxX) maxX = x;
+                        if (y < minY) minY = y;
+                        if (y > maxY) maxY = y;
+                    }
+                }
+            }
+            if (minY == image.Height) minY = 0;
+            if (minX == image.Width) minX = 0;
+            if (maxY == 0) maxY = image.Height;
+            if (maxX == 0) maxX = image.Width;
+            return new CvRect(minX, minY, maxX - minX, maxY - minY);
+        }
+
         public double GetPercentageOfSkinInImage(string filePath, string debugImageFilePath = null)
         {
             using (IplImage imgSrc = new IplImage(filePath))
             {
                 using (CvMemStorage storage = new CvMemStorage())
                 {
-                    CvSeq<CvPoint> points;
+                    CvRect rect;
                     using (IplImage imgGray = new IplImage(filePath, LoadMode.GrayScale))
                     {
-                        imgGray.FindContours(storage, out points, CvContour.SizeOf, ContourRetrieval.External, ContourChain.ApproxNone);
+                        rect = FindBorder(imgGray);
+
                     }
-                    var rect = Cv.BoundingRect(points);
                     if (rect.Width < imgSrc.Width * 0.10 || rect.Height < imgSrc.Height * 0.10)
                         rect = new CvRect(0, 0, imgSrc.Width, imgSrc.Height);
                     using (var subImg = imgSrc.GetSubImage(rect))
